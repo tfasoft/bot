fea22853fb9f11c38a7785b8d0e8e48118b8e6d3
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-26 17:09:05 +0430

    Change uid to tid

--- modules/user.js
+++ modules/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userModule = new Schema({
-    uid: {
+    tid: {
         type: Number,
         required: true
     },
