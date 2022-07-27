9764fad337f3ca8b09e8e2169ce24318662ea495
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-27 21:36:16 +0430

    Commented others and used axios for login

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const { Telegraf } = require('telegraf');
-const mongoose = require('mongoose');
-const randomstring = require('randomstring');
-
-const User = require('./modules/user');
+const axios = require('axios');
 
 require('dotenv').config();
 const env = process.env;
 
 const bot = new Telegraf(env.BOT_TOKEN);
-
-const mdb = `mongodb+srv://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@${env.MONGO_DATABASE}.ji4jf.mongodb.net/${env.MONGO_COLLECTION}?retryWrites=true&w=majority`;
 
 bot.start((ctx) => {
     const message = "I can help you with a modern way to authenticate with Telegram.\nYou can register, login or see your status by clicking the buttons.";
@@ -38,105 +33,82 @@ bot.start((ctx) => {
     };
 
     ctx.reply(message, buttons);
-
-    // mongoose.connect(mdb)
-    //     .then((connection) => {
-    //         User.findOne({tid: ctx.message.from.id})
-    //             .then((result) => {
-    //                 if (result.token === null) {
-    //                     ctx.reply(message, buttons);
-    //                 } else {
-    //                     buttons.reply_markup.inline_keyboard.push([
-    //                         {
-    //                             text: "Get my last token",
-    //                             callback_data: 'last'
-    //                         },
-    //                     ])
-                        
-    //                     ctx.reply(message, buttons);
-    //                 }
-    //             })
-    //             .catch((error) => ctx.reply(error));
-    //     })
-    //     .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
 });
 
 bot.action('login', (ctx) => {
-    mongoose.connect(mdb)
-        .then((connection) => {
-            const newToken = randomstring.generate({length: 25, charset: 'alphabetic'});
+    const data = {
+        tid: ctx.callbackQuery.from.id,
+    }
 
-            User.findOneAndUpdate({tid: ctx.callbackQuery.from.id}, {token: newToken})
-                .then((result) => {
-                    if (result === null) {
-                        ctx.reply('You are not registered.\nPress register button to register.');
-                    } else {
-                        ctx.replyWithHTML(`Your token is:\n<code>${newToken}</code>`);
-                    }
-                })
-                .catch((error) => ctx.reply(error));
+    axios.post(`${env.BACKEND_API}/bot/login`, data)
+        .then((result) => {
+            const data = result.data;
+
+            ctx.replyWithHTML(`Your token is:\n<code>${data.token}</code>`);
         })
-        .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
-bot.action('register', (ctx) => {
-    mongoose.connect(mdb)
-        .then((connection) => {
-            User.findOne({tid: ctx.callbackQuery.from.id})
-                .then((result) => {
-                    if (result === null) {
-                        const user = new User({
-                            tid: ctx.callbackQuery.from.id,
-                            token: randomstring.generate({length: 25, charset: 'alphabetic'})
-                        });
+// bot.action('register', (ctx) => {
+//     mongoose.connect(mdb)
+//         .then((connection) => {
+//             User.findOne({tid: ctx.callbackQuery.from.id})
+//                 .then((result) => {
+//                     if (result === null) {
+//                         const user = new User({
+//                             tid: ctx.callbackQuery.from.id,
+//                             token: randomstring.generate({length: 25, charset: 'alphabetic'})
+//                         });
                     
-                        user.save()
-                            .then((result) => ctx.reply('You are now registered!'))
-                            .catch((error) => ctx.reply(error));
-                    } else {
-                        ctx.reply('You are already registered!');
-                    }
-                })
-                .catch((error) => ctx.reply(error));
-        })
-        .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
-});
+//                         user.save()
+//                             .then((result) => ctx.reply('You are now registered!'))
+//                             .catch((error) => ctx.reply(error));
+//                     } else {
+//                         ctx.reply('You are already registered!');
+//                     }
+//                 })
+//                 .catch((error) => ctx.reply(error));
+//         })
+//         .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
+// });
 
-bot.action('last', (ctx) => {
-    mongoose.connect(mdb)
-        .then((connection) => {
-            User.findOneAndUpdate({tid: ctx.callbackQuery.from.id})
-                .then((result) => {
-                    ctx.replyWithHTML(`Your token is:\n<code>${result.token}</code>`);
-                })
-                .catch((error) => ctx.reply(error));
-        })
-        .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
-});
+// bot.action('last', (ctx) => {
+//     mongoose.connect(mdb)
+//         .then((connection) => {
+//             User.findOneAndUpdate({tid: ctx.callbackQuery.from.id})
+//                 .then((result) => {
+//                     ctx.replyWithHTML(`Your token is:\n<code>${result.token}</code>`);
+//                 })
+//                 .catch((error) => ctx.reply(error));
+//         })
+//         .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
+// });
 
-bot.action('info', (ctx) => {
-    mongoose.connect(mdb)
-        .then((connection) => {
-            let data = `
-        Your information is listed here:
-        - name: ${ctx.callbackQuery.from.first_name}
-        - tid: <code>${ctx.callbackQuery.from.id}</code>
-        - Registration status:
-            `;
+// bot.action('info', (ctx) => {
+//     mongoose.connect(mdb)
+//         .then((connection) => {
+//             let data = `
+//         Your information is listed here:
+//         - name: ${ctx.callbackQuery.from.first_name}
+//         - tid: <code>${ctx.callbackQuery.from.id}</code>
+//         - Registration status:
+//             `;
 
-            User.findOne({tid: ctx.callbackQuery.from.id})
-                .then((result) => {
-                    if (result === null) {
-                        data += 'You are not registed yet.';
-                        ctx.replyWithHTML(data);
-                    } else {
-                        data += 'You are registered.';
-                        ctx.replyWithHTML(data);
-                    }
-                })
-                .catch((error) => ctx.reply(error));
-        })
-        .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
-});
+//             User.findOne({tid: ctx.callbackQuery.from.id})
+//                 .then((result) => {
+//                     if (result === null) {
+//                         data += 'You are not registed yet.';
+//                         ctx.replyWithHTML(data);
+//                     } else {
+//                         data += 'You are registered.';
+//                         ctx.replyWithHTML(data);
+//                     }
+//                 })
+//                 .catch((error) => ctx.reply(error));
+//         })
+//         .catch((error) => ctx.reply('Sorry, server is busy. Press /start again.'));
+// });
 
 bot.launch();
