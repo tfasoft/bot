2890ab55a690d8626517b04ab42d4ce82ff86143
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-06 20:08:28 +0330

    Working

--- bot/controllers/basic/basic.controllers.js
+++ bot/controllers/basic/basic.controllers.js
@@ -1,51 +1,57 @@
 import { API } from "$bot/api/index.js";
 
 export const START = async (ctx) => {
-  const { id } = ctx.message.from;
+  console.log(ctx.startPayload);
 
-  const buttons = [];
+  const payload = "provider-Facebook--service-Authentication";
 
-  try {
-    const { data } = await API.get(`users/${id}`);
+  await ctx.reply(`https://t.me/tfasoft_test_bot?start=${payload}`);
 
-    buttons.push([
-      {
-        text: "Get access token",
-        callback_data: "login",
-      },
-    ]);
-    buttons.push([
-      {
-        text: "My info",
-        callback_data: "info",
-      },
-      {
-        text: "My logins",
-        callback_data: "logs",
-      },
-    ]);
-  } catch (error) {
-    buttons.push([
-      {
-        text: "Register",
-        callback_data: "register",
-      },
-    ]);
-  }
+  // const { id } = ctx.message.from;
 
-  const messages = [
-    "I can help you with a modern way to authenticate with Telegram.",
-    "You can register, login or see your status by clicking the buttons.",
-    "\nFor more information, head over to https://tfasoft.com.",
-  ];
+  // const buttons = [];
 
-  const message = messages.join("\n");
+  // try {
+  //   const { data } = await API.get(`users/${id}`);
 
-  const buttonsMarkup = {
-    reply_markup: {
-      inline_keyboard: buttons,
-    },
-  };
+  //   buttons.push([
+  //     {
+  //       text: "Get access token",
+  //       callback_data: "login",
+  //     },
+  //   ]);
+  //   buttons.push([
+  //     {
+  //       text: "My info",
+  //       callback_data: "info",
+  //     },
+  //     {
+  //       text: "My logins",
+  //       callback_data: "logs",
+  //     },
+  //   ]);
+  // } catch (error) {
+  //   buttons.push([
+  //     {
+  //       text: "Register",
+  //       callback_data: "register",
+  //     },
+  //   ]);
+  // }
 
-  await ctx.reply(message, buttonsMarkup);
+  // const messages = [
+  //   "I can help you with a modern way to authenticate with Telegram.",
+  //   "You can register, login or see your status by clicking the buttons.",
+  //   "\nFor more information, head over to https://tfasoft.com.",
+  // ];
+
+  // const message = messages.join("\n");
+
+  // const buttonsMarkup = {
+  //   reply_markup: {
+  //     inline_keyboard: buttons,
+  //   },
+  // };
+
+  // await ctx.reply(message, buttonsMarkup);
 };
